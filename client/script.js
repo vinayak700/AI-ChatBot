f2c933be312e71e9801ba23c238f312235e660a2
Vinayak Gupta <75876908+vinayak700@users.noreply.github.com>
2023-12-03 13:28:50 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -71,7 +71,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
   window.scrollTo(0, document.body.scrollHeight);
 
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://ai-chatbot-7sgy.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
